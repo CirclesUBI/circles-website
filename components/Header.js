2a80728070f460afefbcf1370191ece9a8569642
Emin <mino@Emins-MacBook-Pro.local>
2020-08-08 20:44:21 +0530

    final tuning

--- components/Header.js
+++ components/Header.js
@@ -6,13 +6,17 @@ import Menu, { SocialMenu } from './Menu';
 const Header = ({ large }) => {
   return (
     <Row style={{ alignItems: 'center' }}>
-      <Col xs={3}>
+      <Col xs={3} md={2}>
         <Box pad={{ top: 'small' }}>
           <Image src="/images/logo.svg" />
         </Box>
       </Col>
-      <Col xs={6}>{large && <Menu activeSection="donate" large />}</Col>
-      <Col xs={3}>{large ? <SocialMenu /> : <Menu large={false} />}</Col>
+      <Col xs={6} md={8}>
+        {large && <Menu activeSection="donate" large />}
+      </Col>
+      <Col xs={3} md={2}>
+        {large ? <SocialMenu /> : <Menu large={false} />}
+      </Col>
     </Row>
   );
 };
