06825a6a0187f17640900a5d96e152c2329ed3fe
Emin <mino@Emins-MacBook-Pro.local>
2020-08-19 23:00:55 +0530

    slider refactoring

--- components/PhoneSlider.js
+++ components/PhoneSlider.js
@@ -25,62 +25,6 @@ const sliderContent = [
   },
 ];
 
-const ImagePhoneBgrd = ({ imageLink, count, caption }) => {
-  return (
-    <Box
-      align="center"
-      pad="medium"
-      margin="large"
-      overflow="hidden"
-      flex={{ grow: 0, shrink: 0 }}
-      wrap
-    >
-      <Box
-        background={{
-          image: `url('/images/phone-frame.svg')`,
-          size: 'contain',
-          repeat: 'no-repeat',
-          position: 'center',
-        }}
-        align="center"
-        pad={{ top: 'large', left: '10px', right: '10px' }}
-        margin={{ bottom: 'large' }}
-        height="380px"
-        width="200px"
-        overflow="hidden"
-      >
-        <Box>
-          <Image
-            src={imageLink}
-            height="100%"
-            fit="cover"
-            style={{
-              borderRadius: 32,
-            }}
-          />
-        </Box>
-      </Box>
-      <Box
-        background="dark-1"
-        style={{ borderRadius: '50%' }}
-        pad="medium"
-        width="60px"
-        height="60px"
-        margin={{ bottom: 'medium' }}
-      >
-        <Text size="large" textAlign="center" weight="bold">
-          {count}
-        </Text>
-      </Box>
-      <Box style={{ maxWidth: 240 }}>
-        <Text weight="bold" size="small" textAlign="center">
-          {caption}
-        </Text>
-      </Box>
-    </Box>
-  );
-};
-
 const SliderImage = ({ imageLink, count, caption }) => (
   <Box align="center" pad="medium">
     <Box margin={{ bottom: 'medium' }} height="medium">
@@ -119,7 +63,14 @@ const PhoneSlider = ({ large }) => {
   if (large) {
     return (
       <Box direction="row" align="start" justify="center" gap="medium">
-        <SpreadImages sliderContent={sliderContent} />
+        {sliderContent.map((slider) => (
+          <SliderImage
+            key={slider.caption}
+            count={slider.count}
+            caption={slider.caption}
+            imageLink={slider.imageLink}
+          />
+        ))}
       </Box>
     );
   }
@@ -127,23 +78,17 @@ const PhoneSlider = ({ large }) => {
   return (
     <Box width="medium" alignSelf="center">
       <Slider {...sliderSettings}>
-        <SpreadImages sliderContent={sliderContent} />
+        {sliderContent.map((slider) => (
+          <SliderImage
+            key={slider.caption}
+            count={slider.count}
+            caption={slider.caption}
+            imageLink={slider.imageLink}
+          />
+        ))}
       </Slider>
     </Box>
   );
 };
-
-const SpreadImages = ({ sliderContent }) => (
-  <Fragment>
-    {sliderContent.map((slider) => (
-      <SliderImage
-        key={slider.caption}
-        count={slider.count}
-        caption={slider.caption}
-        imageLink={slider.imageLink}
-      />
-    ))}
-  </Fragment>
-);
 
 export default PhoneSlider;
