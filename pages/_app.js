1f4ff4b3bf6b750cf40d4eb8c323b4ad858c14fb
Emin <mino@Emins-MacBook-Pro.local>
2020-08-19 22:48:36 +0530

    added back the translations

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,13 @@
 import App from 'next/app';
-// import { appWithTranslation } from '../i18n';
+import { appWithTranslation } from '../i18n';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-// MyApp.getInitialProps = async (appContext) => ({
-//   ...(await App.getInitialProps(appContext)),
-// });
+MyApp.getInitialProps = async (appContext) => ({
+  ...(await App.getInitialProps(appContext)),
+});
 
-// export default appWithTranslation(MyApp);
-export default MyApp;
+export default appWithTranslation(MyApp);
