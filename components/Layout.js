5b36c4a62e477f7760d5786c7289e0cd70eadb67
Emin <mino@Emins-MacBook-Pro.local>
2020-08-21 11:47:15 +0530

    added favicon kind of stuff

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React from 'react';
 import { Grommet } from 'grommet';
 import {
@@ -14,6 +15,28 @@ setConfiguration({ gutterWidth: 0 });
 
 const Layout = (props) => (
   <Grommet theme={theme} {...props}>
+    <Head>
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="theme-color" content="#660F33" />
+    </Head>
     <ScreenClassRender
       render={(screenClass) => {
         const large = ['xxl', 'xl', 'lg'].includes(screenClass);
