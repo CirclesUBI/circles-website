cfce9900e882ad16e28bb4aaa80543c9cb41d6e0
Andreas Dzialocha <x12@adz.garden>
2020-09-17 13:52:50 +0200

    Remove next.config settings for now

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const { nextI18NextRewrites } = require('next-i18next/rewrites');
 const localeSubpaths = {};
 
 module.exports = {
-  basePath: '/circles-website',
-  assetPrefix: '/circles-website/',
   rewrites: async () => nextI18NextRewrites(localeSubpaths),
   publicRuntimeConfig: {
     localeSubpaths,
