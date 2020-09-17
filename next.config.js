04b4cf312f344bca04f9abd647fd57e46a3abe6f
Andreas Dzialocha <x12@adz.garden>
2020-09-17 13:47:45 +0200

    Add relative path for static files to next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const { nextI18NextRewrites } = require('next-i18next/rewrites');
 const localeSubpaths = {};
 
 module.exports = {
+  basePath: '/circles-website',
+  assetPrefix: '/circles-website/',
   rewrites: async () => nextI18NextRewrites(localeSubpaths),
   publicRuntimeConfig: {
     localeSubpaths,
