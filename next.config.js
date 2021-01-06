bc01959fcca009bc2925975812a6d9391dc61ed8
Emin Durak <emin.tuta.io>
2021-01-06 18:51:26 +0300

    automated the last fix

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@ const localeSubpaths = {
   es: 'es',
 };
 
+const langs = ['', '/de', '/es'];
+const pages = ['/', '/faq', '/donate', '/contact', '/terms', '/privacy-policy'];
+
+const pathMap = {};
+langs.forEach((lang) => {
+  pages.forEach((page) => {
+    pathMap[`${lang}${page}`] = { page };
+  });
+});
+
+console.log(pathMap);
+
 module.exports = {
   rewrites: async () => nextI18NextRewrites(localeSubpaths),
   publicRuntimeConfig: {
@@ -14,25 +26,6 @@ module.exports = {
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
   ) {
-    return {
-      '/': { page: '/' },
-      '/faq': { page: '/faq' },
-      '/contact': { page: '/contact' },
-      '/donate': { page: '/donate' },
-      '/terms': { page: '/terms' },
-      '/privacy-policy': { page: '/privacy-policy' },
-      '/de': { page: '/' },
-      '/de/faq': { page: '/faq' },
-      '/de/contact': { page: '/contact' },
-      '/de/donate': { page: '/donate' },
-      '/de/terms': { page: '/terms' },
-      '/de/privacy-policy': { page: '/privacy-policy' },
-      '/es': { page: '/' },
-      '/es/faq': { page: '/faq' },
-      '/es/contact': { page: '/contact' },
-      '/es/donate': { page: '/donate' },
-      '/es/terms': { page: '/terms' },
-      '/es/privacy-policy': { page: '/privacy-policy' },
-    };
+    return pathMap;
   },
 };
