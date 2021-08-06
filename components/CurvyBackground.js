c94ed1d0f76c36c38bfdd3d147adc914a9477f77
Emin Durak <emin.tuta.io>
2021-08-06 20:55:55 +0300

    added curvy background component somewhat working

--- components/CurvyBackground.js
+++ components/CurvyBackground.js
@@ -1,3 +1,4 @@
+import { useLayoutEffect, useRef, useState } from 'react';
 import { Box } from 'grommet';
 
 export default function ({
@@ -5,13 +6,50 @@ export default function ({
   children,
   ...otherProps
 }) {
+  const containerRef = useRef();
+  const [dimensions, setDimensions] = useState({
+    clientWidth: 900,
+    containerHeight: 0,
+  });
+
+  useLayoutEffect(() => {
+    if (document && containerRef.current) {
+      setDimensions({
+        clientWidth: document.body.clientWidth,
+        containerHeight: containerRef.current.offsetHeight,
+      });
+    }
+  }, []);
+  if (!dimensions.clientWidth) {
+    return null;
+  }
+
+  const newHeight = (dimensions.clientWidth * 4) / 3;
+  // Because the svg image's proportions are like this to make it look like a perfect circle.
+
+  const containerHeightNew =
+    newHeight > dimensions.containerHeight
+      ? newHeight
+      : dimensions.containerHeight;
+  // Only use the new value if it's larger and thus necessary
+
+  const heightStyle = {
+    height: containerHeightNew,
+  };
+
   return (
-    <div className="curvy-background-container">
-      <div className="curvy-background-top" />
-      <div style={{ backgroundColor: 'white', marginTop: -120 }}>
-        {children}
-      </div>
-      <div className="curvy-background-bottom" />
-    </div>
+    <Box
+      ref={containerRef}
+      width="100%"
+      background={{
+        image: 'url(/images/curvy-background.svg)',
+        size: dimensions.clientWidth > 600 ? 'contain' : '150%',
+      }}
+      justify="center"
+      style={heightStyle}
+      {...otherProps}
+    >
+      {children}
+    </Box>
   );
 }
